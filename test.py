45b43d04c917365a2a3adde9d739c04a3964e2ef
Al-Kaf <a.o.k.2009@hotmail.com>
2019-06-13 12:04:31 +0300

    alll

--- test.py
+++ test.py
@@ -47,7 +47,7 @@ def trainPerceptronAlgorithm(X, y, learn_rate=0.01, num_epochs=25):
     for i in range(num_epochs):
         # In each epoch, we apply the perceptron step.
         W, b = perceptronStep(X, y, W, b, learn_rate)
-        boundary_lines.append((-W[0] / W[1], -b / W[0]))
+        boundary_lines.append((-W[2] / W[1], -b / W[0]))
     return boundary_lines
 
 
